e33eb0a927e40d132fd693d31a288fdcf64687f2
akshaymairal04 <amairal009.btech2022@mme.nitrr.ac.in>
2025-07-01 00:56:32 +0530

    first commit

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,9 @@
+/** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    "@tailwindcss/postcss": {}, // ✅ use this instead of 'tailwindcss'
+    autoprefixer: {}, // ✅ still needed
+  },
 };
 
 export default config;
